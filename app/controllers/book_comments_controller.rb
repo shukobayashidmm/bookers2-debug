68e47ce3d383d6fb22f396eba8e0676c13e84db8
shukobayashidmm <shu.kobayashi-0627@yahoo.ne.jp>
2023-03-24 06:15:31 +0000

    完成

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,9 +4,13 @@ class BookCommentsController < ApplicationController
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
-    comment.save
+    if comment.save
+    redirect_to book_path(book), notice: '投稿に成功しました'
+    else
     redirect_to book_path(book)
+    end
   end
+
 
 
 
