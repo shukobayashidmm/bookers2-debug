e079bcd4c345eb6be308b92247e4e0cfabe9696d
shukobayashidmm <shu.kobayashi-0627@yahoo.ne.jp>
2023-04-03 11:28:16 +0000

    応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,21 +2,24 @@ class BookCommentsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    @comment = current_user.book_comments.new(book_comment_params)
-    @comment.book_id = @book.id
-    if @comment.save
-    redirect_to book_path(@book), notice: '投稿に成功しました'
-    else
-    redirect_to book_path(@book)
-    end
+    @user = @book.user
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.save
+    @book_comment = BookComment.new
+    #redirect_to book_path(@book), notice: '投稿に成功しました
+    #redirect_to book_path(@book
   end
 
 
 
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @user = @book.user
+    @bookcomment = BookComment.find(params[:id])
+    @bookcomment.destroy
+    @bookcomment = BookComment.new
   end
 
   private
