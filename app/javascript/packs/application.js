2186de307fa8b60adcc75d43a91eb0e304826be3
shukobayashidmm <shu.kobayashi-0627@yahoo.ne.jp>
2023-04-10 08:36:55 +0000

    応用課題9a

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,9 +10,17 @@ import "channels"
 
 import "jquery"
 import "popper.js"
+import "../stylesheets/application"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
-import "../stylesheets/application"
+
+import Raty from "raty.js"
+window.raty = function(elem,opt) {
+  let raty =  new Raty(elem,opt)
+  raty.init();
+  return raty;
+}
+
 
 Rails.start()
 Turbolinks.start()
