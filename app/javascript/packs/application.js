667710944871fa65fd666420b7d2c38e6d212c97
shukobayashidmm <shu.kobayashi-0627@yahoo.ne.jp>
2023-04-14 04:33:47 +0000

    応用課題9a

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require raty.js
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -13,13 +14,6 @@ import "popper.js"
 import "../stylesheets/application"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
-
-import Raty from "raty.js"
-window.raty = function(elem,opt) {
-  let raty =  new Raty(elem,opt)
-  raty.init();
-  return raty;
-}
 
 
 Rails.start()
